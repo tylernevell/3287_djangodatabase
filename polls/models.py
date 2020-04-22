07c1c00c2b0b7cfefda9cb879a9b71336c84190d
tylernevell <tylernevell@gmail.com>
2020-04-22 16:18:24 -0700

    Removed pycache files from app and site

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 # The name of each Field instance (e.g. question_text or pub_date) is the field’s name,
 # in machine-friendly format. You’ll use this value in your Python code, and your
 # database will use it as the column name.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('data published')
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
